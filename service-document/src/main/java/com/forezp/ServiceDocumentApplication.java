fcd3d69f01c673c8364b97b2356352dc3d0e576e
lvhy <18035766093@163.com>
2022-04-10 20:45:29 +0800

    document模块

--- service-document/src/main/java/com/forezp/ServiceDocumentApplication.java
+++ service-document/src/main/java/com/forezp/ServiceDocumentApplication.java
@@ -1,17 +1,12 @@
 package com.forezp;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lenovo
@@ -20,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @EnableFeignClients
 @MapperScan("com.forezp.mapper")
-public class ServiceOrderApplication {
+public class ServiceDocumentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceOrderApplication.class, args);
+		SpringApplication.run(ServiceDocumentApplication.class, args);
 	}
 
 	@Bean
